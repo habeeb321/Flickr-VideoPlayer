2692190fae1cb6ca98716671a6f38cd675e39fb7
habeebu <habeebu@hello24.in>
2024-02-02 19:05:55 +0530

    updated

--- lib/controller/flick_video_controller.dart
+++ lib/controller/flick_video_controller.dart
@@ -2,7 +2,7 @@ import 'package:flick_video_player/flick_video_player.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:video_player/video_player.dart';
 
-class FlickVideoController extends GetxController {
+class FlickVideoController extends GetxService {
   late FlickManager flickManager;
 
   @override
@@ -12,7 +12,6 @@ class FlickVideoController extends GetxController {
         Uri.parse(
             "https://static.videezy.com/system/resources/previews/000/044/249/original/01__2822_29.mp4"),
       ),
-      autoPlay: true,
     );
     super.onInit();
   }
